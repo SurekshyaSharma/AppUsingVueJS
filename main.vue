35f1705d36db8401c11bb3f6896d6f32537f164e
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-01-15 15:29:32 -0600

    Create main.vue

---
+++ main.vue
@@ -0,0 +1,23 @@
+new Vue({
+  el:"#theroot",
+  data: {
+     greeting:'Hello from VueJS',
+     count: 4,
+     email:'',
+     petAnimals:[
+       {name:'Cat'},
+       {name:'Dog'},
+       {name:'Bird'},
+       {name:'Fish'},
+       {name:'Turtle'},
+       {name:'Mice'}
+       ],
+     NewPets:''
+  },
+  methods: {
+    addNew: function() {
+     return this.petAnimals.push({name: this.NewPets})
+    }
+  }
+
+})
