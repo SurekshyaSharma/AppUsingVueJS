b56401889707d599516b2f505e3e2a6ab1632820
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-01-16 15:12:01 -0600

    Create ReusingComponent.vue

---
+++ ReusingComponent.vue
@@ -0,0 +1,34 @@
+
+Vue.component('fruit-list',{
+props:['fruits'],
+	template:'<ul> <li v-for="fruit in fruits">{{fruit.name}}</li></ul>' 
+}),
+
+Vue.component('healthy-fruits',{
+props:['fruits'],
+	template:'<ul> <li v-for="fruit in fruits">{{fruit.name}}</li></ul>' 
+}),
+
+
+app = new Vue({
+el:'.main',
+component:['fruit-list','healthy-fruits'],
+data:{
+	fruits:[
+  	{name:'apple'},
+    {name:'banana'},
+    {name:'cantaloupe'},
+    {name:'guava'}
+  
+  ],
+  newFruit:''
+         
+  },
+  methods:
+  {
+  addFruits: function(){
+  	return this.fruits.push({name:this.newFruit})
+  }
+  }
+
+})
