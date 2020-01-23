1fa7e0f2ac0cd5dcfab7f23480bf91c236c9ccd5
SurekshyaSharma <43008704+SurekshyaSharma@users.noreply.github.com>
2020-01-23 11:20:01 -0600

    Create vue.plan.vue

---
+++ vue.plan.vue
@@ -0,0 +1,19 @@
+  Vue.component('plan', {
+    template: '#plan-template',
+    props: {
+      name: {
+        type: String
+      }
+    }
+  })
+  new Vue({
+    el: '#app',
+    data: {
+      plans: [
+    	 {name:'The Single'},
+       {name:'The Curious'},
+       {name:'The Addict'},
+       {name:'The Smart'}
+       ]
+    }
+  })
